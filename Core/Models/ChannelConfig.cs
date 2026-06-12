332637d3ca05f9af3abd933653002a56bfc4035a
Davide Clemente <1190497@isep.ipp.pt>
2026-06-12 19:38:48 +0100

    Add ChannelConfig model and extend AppSettings for persistence

---
+++ Core/Models/ChannelConfig.cs
@@ -0,0 +1,7 @@
+namespace AudioMixerWin.Core.Models;
+
+public class ChannelConfig
+{
+    public int KnobIndex { get; set; }
+    public string AppName { get; set; } = "";
+}
--- Core/Services/AppSettings.cs
+++ Core/Services/AppSettings.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using AudioMixerWin.Core.Models;
+
 namespace AudioMixerWin.Core.Services;
 
 public class AppSettings
 {
     public string ComPort { get; set; } = "COM3";
     public int BaudRate { get; set; } = 115200;
+    public int RefreshIntervalSeconds { get; set; } = 2;
+    public List<ChannelConfig> Channels { get; set; } = new();
+    public List<string> ExcludedProcesses { get; set; } = new();
 }
